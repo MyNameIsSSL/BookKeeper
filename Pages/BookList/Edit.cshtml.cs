30f1112136f32fcd11cde4010af3c043dfa1f266
blackPigCanFly <remembercharliesmith@hotmail.com>
2020-03-03 21:56:12 +1030

    First commit

---
+++ Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using BookKeeper.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookKeeper
+{
+    public class EditModel : PageModel
+    {
+        private ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task OnGet(int id)
+        {
+            Book = await _db.Book.FindAsync(id);
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        //post handler
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var bookFromDb = await _db.Book.FindAsync(Book.Id);
+                bookFromDb.Name = Book.Name;
+                bookFromDb.Author = Book.Author;
+                bookFromDb.Isbn = Book.Isbn;
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+
+            return RedirectToPage();
+
+        }
+    }
+}
