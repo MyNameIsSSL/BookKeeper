b0322c56af76cfa2906cbc3e72b6c595583d9fc6
blackPigCanFly <remembercharliesmith@hotmail.com>
2020-03-04 21:55:10 +1030

    Done setups

---
+++ Pages/BookList/Upsert.cshtml.cs
@@ -0,0 +1,65 @@
+﻿using System.Threading.Tasks;
+using BookKeeper.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookKeeper
+{
+    public class UpsertModel : PageModel
+    {
+        private ApplicationDbContext _db;
+
+        public UpsertModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            Book = new Book();
+            if (id == null)
+            {
+                // Create
+                return Page();
+            }
+
+            // Update book
+            // find Book based on ID in DB
+            Book = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (Book == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        //post handler
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                if (Book.Id == 0)
+                {
+                    _db.Book.Add(Book);
+                }
+
+                // Update a book property
+                else
+                {
+                    _db.Book.Update(Book);
+                }
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+
+            return RedirectToPage();
+
+        }
+    }
+}
